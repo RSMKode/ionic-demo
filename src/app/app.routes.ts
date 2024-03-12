b7186408df96526f0e2ec0d72431eaf370310c61
Roger Sancho <rogersanchom@gmail.com>
2024-03-12 22:08:31 +0100

    Añadir menú lateral y rutas para la página principal y la lista de productos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    path: 'main',
+    loadComponent: () => import('./main/main.page').then((m) => m.MainPage),
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    path: 'product-list',
+    loadComponent: () => import('./product-list/product-list.page').then( m => m.ProductListPage)
   },
 ];
